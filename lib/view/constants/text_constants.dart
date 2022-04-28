6c2685afd6d05f01a410b8168f3e3649e14ec48f
curite <onthepound@gmail.com>
2022-04-28 19:48:08 +0900

    ナビゲーションタイトルに予定追加と表示

--- lib/view/constants/text_constants.dart
+++ lib/view/constants/text_constants.dart
@@ -1,5 +1,9 @@
 class TextConstants {
+  // カレンダー画面
   static const String calendarViewAppBarTitle = 'カレンダー';
   static const String today = '今日';
   static const String calendarDateFormat = 'yyyy年MM月';
+
+  // 予定の追加画面
+  static const String scheduleCreateViewAppBarTitle = '予定追加';
 }
---
+++ lib/view/view/schedule_create_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:schedule_management_app/view/constants/text_constants.dart';
+
+class ScheduleCreateView extends StatelessWidget {
+  const ScheduleCreateView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(TextConstants.scheduleCreateViewAppBarTitle),
+        centerTitle: true,
+      ),
+    );
+  }
+}
