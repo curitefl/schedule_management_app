9663fbd6f6259fd62ca45ff1f737e83cd32a5e06
curite <onthepound@gmail.com>
2022-04-29 12:09:00 +0900

    予定追加ボタンを表示。最大行数：１

--- lib/view/view/schedule_list_view.dart
+++ lib/view/view/schedule_list_view.dart
@@ -10,11 +10,27 @@ class ScheduleListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SimpleDialog(
-      title: Text(
-        DateFormat(
-          TextConstants.scheduleListViewDateFormat,
-          ScheduleListConstants.scheduleListLocale,
-        ).format(DateTime.now()),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            DateFormat(
+              TextConstants.scheduleListViewDateFormat,
+              ScheduleListConstants.scheduleListLocale,
+            ).format(DateTime.now()),
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.add,
+              color: Colors.blue,
+            ),
+            onPressed: () {
+              // TODO 予定追加画面を表示する
+            },
+          ),
+        ],
       ),
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.all(
