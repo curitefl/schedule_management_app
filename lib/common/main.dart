cad00173295a72d0e06915816470616190986387
curite <onthepound@gmail.com>
2022-04-29 18:07:06 +0900

    ProviderScopeでラップ

--- lib/common/main.dart
+++ lib/common/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:schedule_management_app/view/constants/calendar_constants.dart';
 import 'package:schedule_management_app/view/view/carendar_view.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
